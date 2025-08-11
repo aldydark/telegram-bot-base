acdaec170abf94b3c54597da91ec9b57a034eac0
aldydark <aldydarkerror@gmail.com>
2025-08-11 18:31:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    botToken: '8243982216:AAFk737O8t2oNOQ51fIYKlqrWX-spILiLXo', // Replace with your actual bot token
-    ownerId:  7918330567, // Set your owner ID here
+    botToken: '1111', // Replace with your actual bot token
+    ownerId:  1111, // Set your owner ID here
 };
